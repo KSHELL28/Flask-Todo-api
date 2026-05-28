995a786dcef7b49ca071713c1be69a4e09c35e06
KSHELL28 <vedantshelke35038@gmail.com>
2026-05-28 16:14:33 +0530

    Version 1.1 DS is dict with task and status, other methods modified

--- app.py
+++ app.py
@@ -2,24 +2,41 @@ from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
-tasks = []
+tasks = [
+            {"task" : "DSA" , "status":"To do" },
+            {"task" : "DA" ,"status":"Completed" },
+            {"task" : "A" ,"status":"Completed"}
+]
 
 @app.route("/")
 def welcome():
     return "<h1>Welcome to my App</h1>" 
 
+@app.route("/get_all_tasks",methods=['GET'])
+def get_all_tasks():
+    return jsonify(tasks)
+
 @app.route("/get_tasks",methods=['GET'])
 def get_tasks():
-    return jsonify(tasks)
+    status_ = request.args.get('status') 
+        
+    req_tasks = [] 
+
+    for i in tasks:
+        if(i["status"] == status_):
+            req_tasks.append(i)
+
+    return jsonify(req_tasks)         
 
 @app.route("/add_task",methods=['POST'])
 def add_task():
     data = request.get_json()  # get whatever is written in json 
 
-    data['status'] = "To do"
-    task = data['task']
+    task_ = data['task']
+    status_ = "To do" 
 
-    tasks.append(task)
+    new_task = { "task":task_ , "status":status_}
+    tasks.append(new_task)
 
     return jsonify(
         {
@@ -45,8 +62,8 @@ def remove_task():
         )
 
     for i in tasks: #search for the task 
-        if i == task :
-            tasks.remove(task) 
+        if i['task'] == task :
+            tasks.remove(i) 
             found = True
             break
 
@@ -58,12 +75,28 @@ def remove_task():
                 "Tasks" : tasks  
             }
         )
-    return jsonify(
+    else:
+        return jsonify(
         {
             "message" : "task not found"
         }
     )
     
+@app.route("/change_status/<string:task_name>",methods=['PATCH'])
+def Change_status(task_name):
+    data_ = request.get_json() 
+    status_ = data_['status'] 
+    if(status_ not in ["To do" ,"Completed"]):
+        return jsonify({"message":"enter valid status"})
+
+    for i in tasks:
+        if(i['task'] == task_name): 
+            i['status'] = status_ 
+            return jsonify({
+                "Tasks":tasks,
+                "message":"Status changed successfully"})
+    
+    return jsonify({"message":"Task not found"})
 
 if __name__ == "__main__": 
     app.run(debug=True)
