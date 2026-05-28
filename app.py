47bc7f6f12ec83f435f7eea246f74ee22ee75f36
KSHELL28 <vedantshelke35038@gmail.com>
2026-05-28 16:56:14 +0530

    Version 1.2 , More better and suitable in REST apis

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ tasks = [
 ]
 
 @app.route("/")
+
 def welcome():
     return "<h1>Welcome to my App</h1>" 
 
-@app.route("/get_all_tasks",methods=['GET'])
-def get_all_tasks():
-    return jsonify(tasks)
+@app.route("/tasks",methods=['GET'])
 
-@app.route("/get_tasks",methods=['GET'])
 def get_tasks():
+
     status_ = request.args.get('status') 
-        
+
+    if(status_ is None):
+        return jsonify(tasks)
+                
     req_tasks = [] 
 
     for i in tasks:
@@ -28,7 +30,7 @@ def get_tasks():
 
     return jsonify(req_tasks)         
 
-@app.route("/add_task",methods=['POST'])
+@app.route("/tasks",methods=['POST'])
 def add_task():
     data = request.get_json()  # get whatever is written in json 
 
@@ -45,24 +47,13 @@ def add_task():
         }
     )
 
-@app.route("/remove_task",methods=['DELETE']) 
-def remove_task():
-
-    task_to_delete = request.get_json() #delete karaycha task
+@app.route("/tasks/<string:task_name>",methods=['DELETE']) 
+def remove_task(task_name):
 
     found = False
 
-    task = task_to_delete['task']
-
-    if(task == ""):
-        return jsonify(
-            {
-                "message":"enter valid task"
-            }
-        )
-
     for i in tasks: #search for the task 
-        if i['task'] == task :
+        if i['task'] == task_name :
             tasks.remove(i) 
             found = True
             break
@@ -70,7 +61,7 @@ def remove_task():
     if(found):
         return jsonify(
             {
-                "removed":task ,
+                "removed":task_name ,
                 "message" : "task removed",
                 "Tasks" : tasks  
             }
@@ -82,21 +73,24 @@ def remove_task():
         }
     )
     
-@app.route("/change_status/<string:task_name>",methods=['PATCH'])
-def Change_status(task_name):
+@app.route("/tasks/<string:task_name>",methods=['PATCH'])
+def change_status(task_name):
     data_ = request.get_json() 
+    if(data_['status'] not in ["To do" ,"Completed"]):
+        return jsonify({"message":"enter valid status"}),400
     status_ = data_['status'] 
-    if(status_ not in ["To do" ,"Completed"]):
-        return jsonify({"message":"enter valid status"})
 
     for i in tasks:
         if(i['task'] == task_name): 
             i['status'] = status_ 
             return jsonify({
-                "Tasks":tasks,
-                "message":"Status changed successfully"})
+                "message":"Status changed successfully" ,
+                "Task":task_name,
+                "status":status_
+                }
+                ),200
     
-    return jsonify({"message":"Task not found"})
+    return jsonify({"message":"Task not found"}),404
 
 if __name__ == "__main__": 
     app.run(debug=True)
