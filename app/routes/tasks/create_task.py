4777c4cfb49ddc0fcb7cea4a13c66c4646ed667f
KSHELL28 <vedantshelke35038@gmail.com>
2026-06-19 13:20:01 +0530

    did some changes in auth

--- app/routes/tasks/create_task.py
+++ app/routes/tasks/create_task.py
@@ -1,10 +1,11 @@
 from flask import Blueprint,jsonify,request
-
+from flask_jwt_extended import jwt_required
 from app.services.task.add_task import add_task
 
 add_bp = Blueprint("add_task",__name__)
 
 @add_bp.route("/tasks",methods=['POST'])
+@jwt_required()
 def addTask():
     data = request.get_json()  # get whatever is written in json 
 
